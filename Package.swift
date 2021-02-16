a8e014854700c9d37ffbc10f2e324efa53efc278
Efraim Budusan <efraim.budusan@tapptitude.com>
2021-02-16 14:48:41 +0200

    exclude example files from libarary

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -21,6 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "TTParallaxOnboarding",
-            dependencies: [])
+            dependencies: [],
+            exclude: ["TTParallaxOnboardingExample", "Resources"])
     ]
 )
